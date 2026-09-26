7b17bd6cf5b1f1efdd548431cdb8ae342f399044
Bi1806 <ntruonggiang994@gmail.com>
2026-09-26 12:53:26 +0700

    Update loginform

--- PoolTournament/Forms/LoginForm.Designer.cs
+++ PoolTournament/Forms/LoginForm.Designer.cs
@@ -9,16 +9,19 @@
 
         /// <summary>
         ///  Clean up any resources being used.
-        /// </summary>        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// </summary>
+        /// <param name="disposing">
+        /// true if managed resources should be disposed; otherwise, false.
+        /// </param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
-
         #region Windows Form Designer generated code
 
         /// <summary>
--- PoolTournament/obj/Debug/net10.0-windows/PoolTournament.AssemblyInfo.cs
+++ PoolTournament/obj/Debug/net10.0-windows/PoolTournament.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoolTournament")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80acd90a2904a4161743cfd1b2ebe6a9918c952c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7797e68cdb610089ec314b707b0c082ae46ead4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoolTournament")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoolTournament")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
